0303de7a6eb2d7d2f7160d7175ad66913e8babc9
leopnt <leopnt.dev@gmail.com>
2023-05-17 01:28:08 +0200

    format code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 mod app_config;
-use std::vec;
-
 use app_config::AppConfig;
 
 mod dir_tree;
@@ -12,13 +10,16 @@ fn main() -> Result<(), confy::ConfyError> {
 
     let mut dir_tree = DirTree::from_config(&cfg);
 
-    dir_tree.put_into(
-        vec![
-        "ATTACK".to_string(),
-        "BRIGHT".to_string(),
-        "ELECTRO".to_string()],
-        "track1.mp3".to_string()
-        ).unwrap();
+    dir_tree
+        .put_into(
+            vec![
+                "ATTACK".to_string(),
+                "BRIGHT".to_string(),
+                "ELECTRO".to_string(),
+            ],
+            "track1.mp3".to_string(),
+        )
+        .unwrap();
 
     println!("{}", dir_tree.to_string());
 
