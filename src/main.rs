7f6b8450202afc18b5d33364898559fa7c6ca093
leopnt <leopnt.dev@gmail.com>
2023-07-19 14:54:10 +0200

    add id3 aiff tag seeking

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
-mod app_config;
-use app_config::AppConfig;
+use std::fs::File;
+use std::io::Read;
 
-mod dir_tree;
-use dir_tree::DirTree;
+use std::fmt;
 
-fn main() -> Result<(), confy::ConfyError> {
-    let cfg: AppConfig = confy::load("cmc", None)?;
-    dbg!(&cfg);
+use std::time::Instant;
 
-    let mut dir_tree = DirTree::from_config(&cfg);
+#[derive(Debug)]
+struct Chunk {
+    id: String,
+    size: usize,
+    data: Vec<u8>,
+}
 
-    dir_tree
-        .put_into(
-            vec![
-                "ATTACK".to_string(),
-                "BRIGHT".to_string(),
-                "ELECTRO".to_string(),
-            ],
-            "track1.mp3".to_string(),
-        )
-        .unwrap();
+fn read_aiff_chunk(data: &Vec<u8>, pos: usize) -> Result<Chunk, Box<dyn std::error::Error>> {
+    let id: String = String::from_utf8(data[pos..pos + 4].to_vec())?;
+    let size: usize = i32::from_be_bytes(data[pos + 4..pos + 8].try_into()?) as usize;
+    let data: Vec<u8> = data[pos + 8..pos + 8 + size as usize].to_vec();
 
-    println!("{}", dir_tree.to_string());
+    Ok(Chunk { id, size, data })
+}
+
+fn main() -> std::io::Result<()> {
+    let start_time = Instant::now();
+
+    let mut file = File::open("/Users/leopnt/Music/TCOTC/01 View Source.aiff")?;
+    let mut data = Vec::new();
+    file.read_to_end(&mut data)?;
+
+
+    let mut cursor: usize = 0;
+
+    loop {
+        if cursor >= data.len() {
+            println!("Cursor reached EOF");
+            break;
+        }
+
+        let chunk: Chunk = read_aiff_chunk(&data, cursor).unwrap();
+        println!("cursor: {} {} {}", cursor, chunk.id, chunk.size);
+
+        match chunk.id.as_str() {
+            "FORM" => cursor += 12, // FORM is a container chunk, we jump to the first data byte of this chunk
+            "COMT" => cursor += 8 + chunk.size,
+            "COMM" => cursor += 8 + chunk.size,
+            "SSND" => cursor += 8 + chunk.size,
+            "ID3 " => {
+                println!("Found ID3 Tag!");
+                let elapsed_time = start_time.elapsed();
+                println!("Seeking ID3 took {:?}", elapsed_time);
+                break;
+            }
+            _ => {
+                println!("Unknown identifier");
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
