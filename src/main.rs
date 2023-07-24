3b9c58c5b08c6c5c4797a0375b6b1ab1a5ba0efb
leopnt <leopnt.dev@gmail.com>
2023-07-24 22:30:43 +0200

    add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::path::Path;
 use std::time::Instant;
 
+use clap::Parser;
+
 mod utils;
 
 mod track;
@@ -10,22 +12,34 @@ use track::Track;
 mod track_database;
 use track_database::build_track_database;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The directory to analyse and copy the tracks from
+    #[arg(short, long)]
+    source: String,
+
+    /// The target directory to copy the classified tracks into
+    #[arg(short, long)]
+    target: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start_time = Instant::now();
 
-    let source_folder = Path::new("/Users/leopnt/Desktop/TCOTC_TEST");
-    let target_folder = Path::new("/Users/leopnt/Desktop/TCOTC_TEST_TARGET");
+    let args = Args::parse();
+    let source_folder = Path::new(&args.source);
+    let target_folder = Path::new(&args.target);
 
-    let source_entries = fs::read_dir(&source_folder).unwrap();
-    //let target_entries = fs::read_dir("/Users/leopnt/Desktop/test-comment-music").unwrap();
+    let source_entries = fs::read_dir(source_folder).unwrap();
 
     let source_tracks = build_track_database(source_entries);
+
+    println!("Found {} valid source tracks", source_tracks.len());
 
     for track in source_tracks.iter() {
         track.copy_to_target_paths(target_folder);
     }
-
-    println!("Count {}", source_tracks.len());
 
     let elapsed_time = start_time.elapsed();
     println!("Program took {:?}", elapsed_time);
