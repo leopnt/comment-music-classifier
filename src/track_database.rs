9d096671abb7b3bb6f696694112e5ae780bc4d79
leopnt <leopnt.dev@gmail.com>
2023-07-24 21:24:33 +0200

    add related track name for parsing errors

--- src/track_database.rs
+++ src/track_database.rs
@@ -7,17 +7,20 @@ pub fn build_track_database(entries: fs::ReadDir) -> Vec<Track> {
 
     for entry in entries {
         let entry = entry.unwrap();
-        let path = entry.path();
 
-        if path.is_dir() {
+        if entry.path().is_dir() {
             // If the entry is a directory, recursively call the function
-            let sub_entries = fs::read_dir(path).expect("Failed to read directory");
+            let sub_entries = fs::read_dir(entry.path()).expect("Failed to read directory");
             let sub_tracks = build_track_database(sub_entries);
             tracks.extend(sub_tracks);
         } else if let Ok(track) = Track::from_pathbuf(entry.path()) {
             tracks.push(track);
         } else if let Err(e) = Track::from_pathbuf(entry.path()) {
-            println!("{:?}", e);
+            println!(
+                "Track parsing failed for: {:?} Error is: {:?}",
+                entry.path(),
+                e
+            );
         }
     }
 
